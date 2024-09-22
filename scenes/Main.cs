940b3f2f076627694955af4b143d46d284638774
Hilton Perantunes <hperantunes@gmail.com>
2024-09-21 23:43:40 -0400

    2. C# and VS 2022 Configuration

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -7,7 +7,7 @@ public partial class Main : Node2D
 	public override void _Ready()
 	{
 		GD.Print("Hello world!");
-	}
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
