a87558f7c3902eb2137c82db2d98077897ada0c4
Hilton Perantunes <hperantunes@gmail.com>
2024-09-30 22:43:37 -0400

    26. Introduction to Control Nodes

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Manager;
 using Game.Resources.Building;
+using Game.UI;
 using Godot;
 
 namespace Game;
@@ -10,9 +11,8 @@ public partial class Main : Node
     private Sprite2D cursor;
     private BuildingResource towerResource;
     private BuildingResource villageResource;
-    private Button placeTowerButton;
-    private Button placeVillageButton;
     private Node2D ySortRoot;
+    private GameUI gameUI;
 
     private Vector2I? hoveredGridCell;
     private BuildingResource toPlaceBuildingResource;
@@ -23,14 +23,13 @@ public partial class Main : Node
         villageResource = GD.Load<BuildingResource>("res://resources/building/village.tres");
         gridManager = GetNode<GridManager>("GridManager");
         cursor = GetNode<Sprite2D>("Cursor");
-        placeTowerButton = GetNode<Button>("PlaceTowerButton");
-        placeVillageButton = GetNode<Button>("PlaceVillageButton");
         ySortRoot = GetNode<Node2D>("YSortRoot");
+        gameUI = GetNode<GameUI>("GameUI");
 
         cursor.Visible = false;
 
-        placeTowerButton.Pressed += OnPlaceTowerButtonPressed;
-        placeVillageButton.Pressed += OnPlaceVillageButtonPressed;
+        gameUI.PlaceTowerButtonPressed += OnPlaceTowerButtonPressed;
+        gameUI.PlaceVillageButtonPressed += OnPlaceVillageButtonPressed;
         gridManager.ResourceTilesUpdated += OnResourceTilesUpdated;
     }
 
