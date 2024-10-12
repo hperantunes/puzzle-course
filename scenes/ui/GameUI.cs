150450c127d7a4e949020e749ce93e4809cfbdca
Hilton Perantunes <hperantunes@gmail.com>
2024-10-11 22:03:26 -0400

    52. Styling Building Selection UI

--- scenes/ui/GameUI.cs
+++ scenes/ui/GameUI.cs
@@ -5,30 +5,31 @@ namespace Game.UI;
 
 public partial class GameUI : CanvasLayer
 {
-    private HBoxContainer hBoxContainer;
+    private VBoxContainer buildingSectionContainer;
 
     [Signal]
     public delegate void BuildingResourceSelectedEventHandler(BuildingResource buildingResource);
 
     [Export]
     private BuildingResource[] buildingResources;
+    [Export]
+    private PackedScene buildingSectionScene;
 
     public override void _Ready()
     {
-        hBoxContainer = GetNode<HBoxContainer>("MarginContainer/HBoxContainer");
-        CreateBuildingButtons();
+        buildingSectionContainer = GetNode<VBoxContainer>("%BuildingSectionContainer");
+        CreateBuildingSections();
     }
 
-    private void CreateBuildingButtons()
+    private void CreateBuildingSections()
     {
         foreach (var buildingResource in buildingResources)
         {
-            var buildingButton = new Button
-            {
-                Text = $"Place {buildingResource.DisplayName}"
-            };
-            hBoxContainer.AddChild(buildingButton);
-            buildingButton.Pressed += () => EmitSignal(SignalName.BuildingResourceSelected, buildingResource);
+            var buildingSection = buildingSectionScene.Instantiate<BuildingSection>();
+            buildingSectionContainer.AddChild(buildingSection);
+
+            buildingSection.SetBuildingResource(buildingResource);
+            buildingSection.SelectButtonPressed += () => EmitSignal(SignalName.BuildingResourceSelected, buildingResource);
         }
     }
 }
